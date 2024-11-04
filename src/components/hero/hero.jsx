34e646352111f1cf07ea443c8163c218685cabd7
Ian Miriti <ianklaus17@gmail.com>
2024-11-04 16:30:27 +0300

    commented

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,7 +1,7 @@
 import './Hero.css'
 import arrow_btn from '../../Assets/arrow_btn.png'
-import play_icon from '../../Assets/play_icon.png'
-import pause_icon from '../../Assets/pause_icon.png'
+//import play_icon from '../../Assets/play_icon.png'
+//import pause_icon from '../../Assets/pause_icon.png'
 
 const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
   return (
